c510c92a3957321014531bf644ac8435c84a5f2e
HumanBojack <spychalar59@gmail.com>
2022-01-31 16:30:28 +0100

    channels list, channel and new_channel

--- api_demo.py
+++ api_demo.py
@@ -1,13 +1,41 @@
+from nturl2path import url2pathname
 from flask import Flask, request, json
 from constant.http_status_codes import *
+from exercice import channel_dictionary
 
 api = Flask(__name__)
 
 
-@api.route("/helloworld/")
-def hello_get():
-    return {"data":"Hello Charles"}
+@api.route("/channels/list")
+def list_channels():
+    return {"data": [x for x in channel_dictionary]}
 
+@api.route("/channels/list/<string:name>")
+def specific_channels(name):
+    return {"data": channel_dictionary[name]}
+
+@api.route("/channels/new", methods=["POST"])
+def add_channel():
+    title = request.json.get("title")
+    description = request.json.get("description")
+    language = request.json.get("language")
+    url = request.json.get("url")
+    subjects = request.json.get("subjects")
+    image_url = request.json.get("image_url")
+
+    if all(x != "" and x is not None for x in [title, description, language, url, subjects, image_url]):
+        if isinstance(subjects, list):
+            new_dict = {
+                "title": title,
+                "description": description,
+                "language": language,
+                "url": url,
+                "subjects": subjects,
+                "image_url": image_url
+                }
+            channel_dictionary[title] = new_dict
+            return new_dict
+    return {"error": HTTP_406_NOT_ACCEPTABLE}, HTTP_406_NOT_ACCEPTABLE
 
 @api.route("/helloworld/", methods=["POST"])
 def hello_post():

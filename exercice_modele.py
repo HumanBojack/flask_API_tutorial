04003bca4c0564ae6c126f764c6fed29f2b10058
charles-42 <71760090+charles-42@users.noreply.github.com>
2022-02-01 15:13:28 +0100

    update exercice

--- exercice_modele.py
+++ exercice_modele.py
@@ -9,15 +9,15 @@ Prérequis:
 
 Vous devez compléter l'API_modele_dem avec:
 - un nouveau modèle représantant la table video (titre, longueur, channel) et ajouter des lignes à la base de données
-- une méthode (requete d'API) pour obtenir toutes les vidéos d'une chaine
-- une méthode pour obtenir tous les attributs d'une vidéo
-- une méthode pour créer une vidéo
-- une méthode pour supprimer une vidéo
-- une méthode pour modifier le titre d'une vidéo
+- une vue (creer une vue de l'API) pour obtenir toutes les vidéos d'une chaine
+- une vue pour obtenir tous les attributs d'une vidéo
+- une vue pour créer une vidéo
+- une vue pour supprimer une vidéo
+- une vue pour modifier le titre d'une vidéo
 
 
 - une notion d'utilisateur (nom, email) qui possède une liste de chaines favorites
-- une méthode pour obtenir toutes les vidéos favorites d'un utilisateur
+- une vue pour obtenir toutes les vidéos favorites d'un utilisateur
 
 Bonus:
 un système de login et de token pour protéger la route delete video 

98f33df119961b31d941cc99813d21375f20afd4
HumanBojack <spychalar59@gmail.com>
2022-06-20 10:47:01 +0200

    Login template

--- application/views.py
+++ application/views.py
@@ -10,7 +10,6 @@ def get_channels():
     channel_list=[]
     for channel in Channel.query.all():
         channel_list.append(channel.title)
-    # return {"channels_list" : channel_list}
     return render_template("channel_list.html", channel_list=channel_list)
 
 
@@ -18,6 +17,7 @@ def get_channels():
 def get_single_channel(channel):
     query_result = Channel.find_by_title(title = channel)
     return { channel : query_result.json() }
+
 
 @app.post("/channels/")
 def post_new_channel():
@@ -45,38 +45,25 @@ def post_new_channel():
             "nouvelle chaine" : request.form
             }
 
+
 @app.route("/channels/<string:channel>/videos", methods=["GET"])
 def get_channel_videos(channel):
     query_results = Channel.find_by_title(title=channel).videos
     return {channel: [x.title for x in query_results]}
+
 
 @app.route("/videos/<int:id>", methods=["GET"])
 def get_video(id):
     video = Video.query.get_or_404(id)
     return video.json()
 
+
 @app.route("/videos/new", methods=["POST"])
 def new_video():
-    # has channel, length, title
-    # if request.form is None:
-    #     return "hy"
-
-    # channel = request.form.get("channel_id")
-    # length = request.form.get("length")
-    # title = request.form.get("title")
 
     try:
         Video(**request.form).save_to_db()
         return jsonify({"success": f"{request.form.get('title')} added to the database"})
     except:
         return jsonify({"error": "can't add to db"}), HTTP_406_NOT_ACCEPTABLE
-    
-    # if any([x is None for x in [channel, length, title]]):
-    #     return jsonify({"error": "form is not good"}), HTTP_406_NOT_ACCEPTABLE
-
-    # if set(request.form.keys()) != set(["channel_id","length","title"]):
-    #     return jsonify({'error': 'Wrong key'}), HTTP_400_BAD_REQUEST
-
-    # if request.form.get("title") in [video.title for video in Video.query.all()]:
-    #     return jsonify({"error": "This video title is already in our database"}), HTTP_400_BAD_REQUEST
 

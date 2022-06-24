438877d35567f278600485153479851b684d879e
HumanBojack <spychalar59@gmail.com>
2022-06-24 11:29:32 +0200

    Added application insights to the app

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,14 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from applicationinsights import AppInsights
 
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 db = SQLAlchemy(app)
+appinsights = AppInsights(app)
 
 
 from application import routes
@@ -22,3 +23,8 @@ login_manager.init_app(app)
 @login_manager.user_loader
 def load_user(user_id):
   return models.User.query.get(user_id)
+
+@app.after_request
+def after_request(response):
+  appinsights.flush()
+  return response
--- config.py
+++ config.py
@@ -6,6 +6,8 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 OAUTH_CREDENTIALS = {
-  'id': os.environ.get('OAUTH_GOOGLE_ID'),
-  'secret': os.environ.get('OAUTH_GOOGLE_SECRET')
-}
+        'id': os.environ.get('OAUTH_GOOGLE_ID'),
+        'secret': os.environ.get('OAUTH_GOOGLE_SECRET')
+        }
+
+APPINSIGHTS_INSTRUMENTATIONKEY = os.environ.get('APPINSIGHTS_INSTRUMENTATIONKEY')

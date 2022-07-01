37e818f8fc9d8e2d9fba5e91d2d3a4466a8a6e43
HumanBojack <spychalar59@gmail.com>
2022-07-01 15:04:45 +0200

    changed firefox imports to chrome

--- selenium_test.py
+++ selenium_test.py
@@ -5,8 +5,8 @@ from helpers import random_letters
 
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 # from webdriver_manager
 import requests
